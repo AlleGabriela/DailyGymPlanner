fe26f60fcd0cecf30a4c80be5c392e0298ad4503
Ale <alexandra.g.fratila@gmail.com>
2023-03-23 21:44:47 +0200

    Curatare

--- lib/core/app_export.dart
+++
@@ -1,8 +0,0 @@
-// export 'package:daily_gym_planner/core/constants/constants.dart';
-export 'package:daily_gym_planner/core/utils/image_constant.dart';
-export 'package:daily_gym_planner/core/utils/color_constant.dart';
-export 'package:daily_gym_planner/core/utils/size_utils.dart';
-export 'package:daily_gym_planner/widgets/custom_image_view.dart';
-export 'package:daily_gym_planner/theme/app_style.dart';
-export 'package:daily_gym_planner/theme/app_decoration.dart';
-export 'package:daily_gym_planner/routes/app_routes.dart';
