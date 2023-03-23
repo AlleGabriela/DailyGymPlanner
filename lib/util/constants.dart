e860bed255458195a1c5d07baa62d5f2fe58f293
IulianaGheorghe <iuliana.gheorghe@student.upt.ro>
2023-03-23 22:51:44 +0200

    DGP-22 : Completed first page design

---
+++ lib/util/constants.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+// Color constants
+const primaryColor = Color(0xff54026e);
+
+
+// App sizing
+const marginSize = 20.0;
+const titleSize = 50.0;
+const buttonHeight = 50.0;
+const buttonWidth = 200.0;
+const buttonText = 25.0;
+
+
+/* -- Strings -- */
+
+// -- Welcome Screen Images (also used in Login & SignUp)
+const String welcomeImage = "assets/images/welcome_image.png";
+const String barbellImage = "assets/images/barbell.png";
+
+// -- Fonts
+const String font1 = "Righteous";
+const String font2 = "Lobster";
