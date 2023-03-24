b43ab984f5305e20793f56ef8c4656b8d42debab
Ale <alexandra.g.fratila@gmail.com>
2023-03-24 22:31:08 +0200

    DGP-28
    Implement box design.

---
+++ lib/util/components_theme/box.dart
@@ -0,0 +1,28 @@
+import 'package:daily_gym_planner/util/constants.dart';
+import 'package:flutter/material.dart';
+
+class Box {
+  InputDecoration textInputDecoration([String lableText="", String hintText = ""]){
+    return InputDecoration(
+      labelText: lableText,
+      hintText: hintText,
+      fillColor: Colors.white,
+      filled: true,
+      contentPadding: EdgeInsets.fromLTRB(marginSize, marginSize, marginSize, marginSize),
+      focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(100.0), borderSide: BorderSide(color: Colors.grey)),
+      enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(100.0), borderSide: BorderSide(color: Colors.grey.shade400)),
+      errorBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(100.0), borderSide: BorderSide(color: Colors.red, width: 2.0)),
+      focusedErrorBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(100.0), borderSide: BorderSide(color: Colors.red, width: 2.0)),
+    );
+  }
+
+  BoxDecoration inputBoxDecorationShaddow() {
+    return BoxDecoration(boxShadow: [
+      BoxShadow(
+        color: Colors.black.withOpacity(0.1),
+        blurRadius: 20,
+        offset: const Offset(0, 5),
+      )
+    ]);
+  }
+}
