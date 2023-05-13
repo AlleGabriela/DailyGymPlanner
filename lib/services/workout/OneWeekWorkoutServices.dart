fb44626f2f92f128ea2047892ae2d8af023d012e
Ale <alexandra.g.fratila@gmail.com>
2023-05-13 16:33:28 +0300

    DGP-60
    DataBase functions for One Week Plan.

---
+++ lib/services/workout/OneWeekWorkoutServices.dart
@@ -0,0 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:daily_gym_planner/services/workout/OneDayWorkoutServices.dart';
+
+
+class OneWeekWorkoutPlan {
+  String userID;
+  String weekPlanName;
+  String monday;
+  String tuesday;
+  String wednesday;
+  String thursday;
+  String friday;
+  String saturday;
+  String sunday;
+
+  OneWeekWorkoutPlan(this.userID, this.weekPlanName, this.monday, this.tuesday, this.wednesday, this.thursday, this.friday, this.saturday, this.sunday);
+
+  Future<void> addOneWeekWorkoutToFirestore() async {
+    final firestore = FirebaseFirestore.instance;
+    String workoutsCategory = "One Week Workout Plan";
+
+    try {
+      final meals = firestore.collection("trainers").doc(userID).collection("workouts");
+      meals.doc(workoutsCategory).set({});
+
+      await firestore.collection("trainers")
+          .doc(userID)
+          .collection("workouts")
+          .doc(workoutsCategory)
+          .collection("One Week Workout Plan")
+          .add({'name': weekPlanName,
+        'monday':await getOneDayWorkoutReference(monday),
+        'tuesday': await getOneDayWorkoutReference(tuesday),
+        'wednesday': await getOneDayWorkoutReference(wednesday),
+        'thursday': await getOneDayWorkoutReference(thursday),
+        'friday': await getOneDayWorkoutReference(friday),
+        'saturday': await getOneDayWorkoutReference(saturday),
+        'sunday': await getOneDayWorkoutReference(sunday),
+        'createdAt': DateTime.now().toUtc()});
+
+    } catch (e) {
+      throw Exception('One Week Meal Plan cannot be added to firebase: $e');
+    }
+  }
+}
