f13bd86b0f20fc9fbe27d923521944061b8e5a30
Ale <alexandra.g.fratila@gmail.com>
2023-05-23 22:25:35 +0300

    DGP-36
    Make the data to be loaded more quick.

--- lib/routes/customer/CustomerHomePage.dart
+++ lib/routes/customer/CustomerHomePage.dart
@@ -20,13 +20,15 @@ class CustomerHomePage extends State<CustomerHome> {
   String trainerLocation = "";
   String trainerPhoto = "";
 
+  late Future<Map<String, String>> fetchDetails;
+
   @override
   void initState() {
     super.initState();
-    _getUserDetails();
+    fetchDetails = _getUserDetails();
   }
 
-  Future<void> _getUserDetails() async {
+  Future<Map<String, String>> _getUserDetails() async {
     FirebaseAuthMethods _authService = FirebaseAuthMethods();
     User? user = FirebaseAuth.instance.currentUser;
     String? email = user?.email;
@@ -37,15 +39,16 @@ class CustomerHomePage extends State<CustomerHome> {
       String nameTrainer = await _authService.getTrainerDetails(trainer, "name");
       String locationTrainer = await _authService.getTrainerDetails(trainer, "location");
       String photoTrainer = await _authService.getTrainerDetails(trainer, "photo");
-      setState(() {
-        userName = name;
-        trainerID = trainer;
-        trainerName = nameTrainer;
-        trainerEmail = emailTrainer;
-        trainerLocation = locationTrainer;
-        trainerPhoto = photoTrainer;
-      });
+      return {
+        'userName': name,
+        'trainerID': trainer,
+        'trainerName': nameTrainer,
+        'trainerEmail': emailTrainer,
+        'trainerLocation': locationTrainer,
+        'trainerPhoto': photoTrainer,
+      };
     }
+    return {}; // Return an empty map if no user details are available
   }
 
   @override
@@ -61,68 +64,155 @@ class CustomerHomePage extends State<CustomerHome> {
           slivers: <Widget>[
             MyAppBar(),
             SliverToBoxAdapter(
-              child: Container(
-                alignment: Alignment.center,
-                margin: const EdgeInsets.all(20),
-                height: 150,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: lightLila,
-                  borderRadius: BorderRadius.circular(30),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: const Offset(0, 2),
-                    ),
-                  ],
-                ),
-                child: Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: CircleAvatar(
-                        radius: 60,
-                        backgroundImage: NetworkImage(trainerPhoto),
+              child: FutureBuilder<Map<String, String>>(
+                future: fetchDetails,
+                builder: (BuildContext context, AsyncSnapshot<Map<String, String>> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Container(
+                      alignment: Alignment.center,
+                      margin: const EdgeInsets.all(20),
+                      height: 150,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: lightLila,
+                        borderRadius: BorderRadius.circular(30),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(0, 2),
+                          ),
+                        ],
                       ),
-                    ),
-                    Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                      child: CircularProgressIndicator(), // Display a loading indicator
+                    );
+                  } else if (snapshot.hasError) {
+                    return Container(
+                      alignment: Alignment.center,
+                      margin: const EdgeInsets.all(20),
+                      height: 150,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: lightLila,
+                        borderRadius: BorderRadius.circular(30),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(0, 2),
+                          ),
+                        ],
+                      ),
+                      child: Text(
+                        'Error: ${snapshot.error}',
+                        style: TextStyle(
+                          color: buttonTextColor,
+                          fontSize: questionSize,
+                          fontFamily: font2,
+                        ),
+                      ), // Display an error message
+                    );
+                  } else if (snapshot.hasData) {
+                    final data = snapshot.data!;
+                    final trainerName = data['trainerName'];
+                    final trainerEmail = data['trainerEmail'];
+                    final trainerLocation = data['trainerLocation'];
+                    final trainerPhoto = data['trainerPhoto'];
+
+                    return Container(
+                      alignment: Alignment.center,
+                      margin: const EdgeInsets.all(20),
+                      height: 150,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: lightLila,
+                        borderRadius: BorderRadius.circular(30),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(0, 2),
+                          ),
+                        ],
+                      ),
+                      child: Row(
                         children: [
-                          Text(
-                            "Name: $trainerName",
-                            style: TextStyle(
-                              color: buttonTextColor,
-                              fontSize: questionSize,
-                              fontFamily: font2,
+                          Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: CircleAvatar(
+                              radius: 60,
+                              backgroundImage: NetworkImage(trainerPhoto!),
                             ),
                           ),
-                          SizedBox(height: 4),
-                          Text(
-                            "E-mail: $trainerEmail",
-                            style: TextStyle(
-                              color: buttonTextColor,
-                              fontSize: questionSize,
-                              fontFamily: font2,
-                            ),
-                          ),
-                          SizedBox(height: 4),
-                          Text(
-                            "Location: $trainerLocation",
-                            style: TextStyle(
-                              color: buttonTextColor,
-                              fontSize: questionSize,
-                              fontFamily: font2,
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Name: $trainerName",
+                                  style: TextStyle(
+                                    color: buttonTextColor,
+                                    fontSize: questionSize,
+                                    fontFamily: font2,
+                                  ),
+                                ),
+                                SizedBox(height: 4),
+                                Text(
+                                  "E-mail: $trainerEmail",
+                                  style: TextStyle(
+                                    color: buttonTextColor,
+                                    fontSize: questionSize,
+                                    fontFamily: font2,
+                                  ),
+                                ),
+                                SizedBox(height: 4),
+                                Text(
+                                  "Location: $trainerLocation",
+                                  style: TextStyle(
+                                    color: buttonTextColor,
+                                    fontSize: questionSize,
+                                    fontFamily: font2,
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
                         ],
                       ),
-                    ),
-                  ],
-                ),
+                    );
+                  } else {
+                    return Container(
+                      alignment: Alignment.center,
+                      margin: const EdgeInsets.all(20),
+                      height: 150,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: lightLila,
+                        borderRadius: BorderRadius.circular(30),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(0, 2),
+                          ),
+                        ],
+                      ),
+                      child: Text(
+                        'No data available',
+                        style: TextStyle(
+                          color: buttonTextColor,
+                          fontSize: questionSize,
+                          fontFamily: font2,
+                        ),
+                      ), // Handle case when no data is available
+                    );
+                  }
+                },
               ),
             ),
             SliverFillRemaining(
@@ -133,5 +223,4 @@ class CustomerHomePage extends State<CustomerHome> {
       ),
     );
   }
-
 }
